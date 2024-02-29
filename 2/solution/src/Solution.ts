f853aba4086e1093cb1f9c19dbf88f5657b095ef
br0wnD3v <bothereum@protonmail.com>
2024-02-29 18:47:23 +0530

    Pushing stable tests soon.

---
+++ 2/solution/src/Solution.ts
@@ -0,0 +1,84 @@
+import {
+  SmartContract,
+  State,
+  state,
+  Field,
+  Struct,
+  method,
+  Bool,
+  UInt64,
+  Provable,
+} from 'o1js';
+
+export class MessageDetails extends Struct({
+  AgentID: UInt64,
+  AgentXLocation: UInt64,
+  AgentYLocation: UInt64,
+  CheckSum: UInt64,
+}) {}
+
+export class Message extends Struct({
+  MessageNumber: Field,
+  Details: MessageDetails,
+}) {}
+
+export class Messages extends Struct({
+  array: Provable.Array(Message, 200),
+}) {}
+
+export class Solution extends SmartContract {
+  @state(Field) HighestValidMessageId = State<Field>();
+
+  @method receive(messages: Messages) {
+    this.HighestValidMessageId.getAndRequireEquals();
+
+    for (let i = 0; i < 200; i++) {
+      const validMessage: Bool = this.validateMessage(messages.array[i]);
+
+      const currH = this.HighestValidMessageId.get();
+      const currMID = messages.array[i].MessageNumber;
+
+      const toSet: Field = Provable.if(
+        currMID.greaterThan(currMID).and(validMessage),
+        currMID,
+        currH
+      );
+
+      this.HighestValidMessageId.set(toSet);
+    }
+  }
+
+  @method validateMessage(message: Message): Bool {
+    const currentAgent: UInt64 = message.Details.AgentID;
+    const currentX: UInt64 = message.Details.AgentXLocation;
+    const currentY: UInt64 = message.Details.AgentYLocation;
+    const CheckSum: UInt64 = message.Details.CheckSum;
+
+    const validAgent: Bool = currentAgent
+      .greaterThanOrEqual(UInt64.from(0))
+      .and(currentAgent.lessThanOrEqual(UInt64.from(3000)));
+
+    const validX: Bool = currentX
+      .greaterThanOrEqual(UInt64.from(0))
+      .and(currentX.lessThanOrEqual(UInt64.from(15000)));
+
+    const validY: Bool = currentY
+      .greaterThanOrEqual(UInt64.from(5000))
+      .and(currentY.lessThanOrEqual(UInt64.from(20000)));
+
+    const validChecksum: Bool = CheckSum.equals(
+      currentAgent.add(currentX).add(currentY)
+    );
+    const validXYRelation: Bool = currentY.greaterThan(currentX);
+
+    const valid_1: Bool = Bool(currentAgent.equals(UInt64.from(0)));
+    const valid_2: Bool = validAgent
+      .and(validX)
+      .and(validY)
+      .and(validChecksum)
+      .and(validXYRelation);
+    const valid: Bool = valid_1.or(valid_2);
+
+    return valid;
+  }
+}
